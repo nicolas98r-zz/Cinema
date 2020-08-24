b9149c26ff49e35ac249d1f0996c4c232ce950ac
nicolasr <nico98.snrm@gmail.com>
2020-08-24 00:24:54 -0500

    Añadida Ventana Inicio

---
+++ vista/InicioUI.java
@@ -0,0 +1,27 @@
+package vista;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class InicioUI extends JFrame{
+    private JPanel mainPanel;
+    private JButton btn1;
+    private JButton btn2;
+    private JButton btn3;
+    private JPanel menuPanel;
+    private JPanel infoPanel;
+    private JLabel txtTitulo;
+
+    public InicioUI() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(507,403);
+        this.setResizable(false);
+        this.setLocationRelativeTo(null);
+        this.setContentPane(mainPanel);
+    }
+
+    public static void main(String[] args) {
+        JFrame f1 = new InicioUI();
+        f1.setVisible(true);
+    }
+}
